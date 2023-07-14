9c65361d2658a21bc868a88fe07c37a6b0dac3ae
mikungg <mikhael.sappy@gmail.com>
2023-07-14 18:30:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pemrograman-Berbasis-Objek SEM 3
+ Repo buat PBO Semester 3
---
+++ nyoba/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace nyoba;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("HELLO, MIMIIIII ♡♡");
+    }
+}
